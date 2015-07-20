1f4783393921273693daed0b1032d757ab1aa558
Manas Mudbari <manasmudbari@Manass-MacBook-Air.local>
2015-07-19 23:17:23 -0700

    script changes

--- facebook.js
+++ facebook.js
@@ -1,15 +1,5 @@
-			window.fbAsyncInit = function() {
-				FB.init({
-					appId      : '808793492569691',
-					xfbml      : true,
-					version    : 'v2.4'
-				});
-			};
-			
-			(function(d, s, id){
-				var js, fjs = d.getElementsByTagName(s)[0];
-     			if (d.getElementById(id)) {return;}
-     			js = d.createElement(s); js.id = id;
-     			js.src = "//connect.facebook.net/en_US/sdk.js";
-     			fjs.parentNode.insertBefore(js, fjs);
-     		}(document, 'script', 'facebook-jssdk'));
+    function fbShare(url, title, descr, image, winWidth, winHeight) {
+        var winTop = (screen.height / 2) - (winHeight / 2);
+        var winLeft = (screen.width / 2) - (winWidth / 2);
+        window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[summary]=' + descr + '&p[url]=' + url + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
+    }
